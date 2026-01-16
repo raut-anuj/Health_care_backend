b50a0c31dc28a6c9dcf2231eb19bf2c27bf7c621
Anuj Kumar <personeluse2000@gmail.com>
2026-01-16 23:58:32 +0530

    Add model definitions for Doctor, Hospital, Medical, and Patient

---
+++ src/models/doctor.model.js
@@ -0,0 +1,51 @@
+
+
+
+import mongoose from "mongoose"
+
+const doctorSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    salary:{
+        type:Number,
+        required:true,
+    },
+    qualification:{
+        type:String,
+        required:true,
+    },
+    experinceinYears:{
+        type:Number,
+        default:0
+    },
+    worksinHospital:
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Hospital"
+        },
+    address:{
+        type:String,
+        required:true
+    },
+    bloodgroup:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    sex:{
+        type:String,
+        enum:["Male","Female","Other"],
+        required:true
+    },
+    number:{
+      type:Number,
+      required:true
+    }
+},{timestamps:true})
+
+export const doctor=mongoose.model("doctor",doctorSchema)
